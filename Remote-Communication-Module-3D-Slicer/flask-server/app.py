e8be030b283e1b345c2f1a4be29dbbfcc8596243
Salo7a <salo7a98x@gmail.com>
2021-02-09 23:51:14 +0200

    Added The Server's Slice Receiving Functionality For Cropping

--- Remote-Communication-Module-3D-Slicer/flask-server/app.py
+++ Remote-Communication-Module-3D-Slicer/flask-server/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, after_this_request
+from flask import Flask, request, after_this_request, flash, redirect, url_for
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -16,6 +18,7 @@ def hello_world():
     allow_CORS()
     return 'Hello World!!!'
 
+
 @app.route('/process')
 def calculate_caScore():
     allow_CORS()
@@ -23,6 +26,32 @@ def calculate_caScore():
 
     print("Done.")
     return "CaScore: BlahBlah"
+
+
+@app.route('/crop', methods=['POST'])
+def GetSlice():
+    if request.method == 'POST':
+        # Names of Received Slices
+        # Names = ["Ax1", "Ax2", "Ax3", "Sag1", "Sag2", "Sag3", "Cor1", "Cor2", "Cor3"]
+        # print(request.files)
+        # Get first Axial Slice Data & Shift Value
+        file = request.files['Ax1']
+        shift = request.form['Ax1']
+        # print(request.form)
+        # Open Slice & reset shift
+        # print(file)
+        if file:
+            Slice = Image.open(file)
+            SliceArray = np.array(Slice, dtype="int16")
+            # print(SliceArray)
+            SliceArray += int(shift)
+            # print(SliceArray)
+            # print(file.filename)
+            # print(shift)
+            # print(SliceArray.shape)
+            # print(SliceArray.max())
+            # print(SliceArray.min())
+    return "Good"
 
 
 if __name__ == '__main__':
