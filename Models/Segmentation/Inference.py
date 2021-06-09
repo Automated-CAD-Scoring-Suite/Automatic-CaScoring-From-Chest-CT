5fbb5a07bf87df8cecc519d20a035db4d0a2287a
Ahmad <ahmedmageed19@gmail.com>
2021-06-10 01:19:11 +0200

    HardNet Inference Added

---
+++ Models/Segmentation/Inference.py
@@ -0,0 +1,80 @@
+#
+# Inference.py
+# Author: Ahmad Abdalmageed
+# Date: 6/10/21
+#
+
+import numpy as np
+import torch
+from scipy.ndimage import zoom
+import SimpleITK as sitk
+
+
+def load_itk(filename):
+    """
+    This funciton reads a '.mhd' file using SimpleITK and return the image array, origin and spacing of the image.
+
+    :param filename:
+    :return:
+    """
+    # Reads the image using SimpleITK
+    itkimage = sitk.ReadImage(filename)
+
+    # Convert the image to a  numpy array first and then shuffle the dimensions to get axis in the order z,y,x
+    ct_scan = sitk.GetArrayFromImage(itkimage)
+
+    # Read the origin of the ct_scan, will be used to convert the coordinates from world to voxel and vice versa.
+    origin = np.array(list(reversed(itkimage.GetOrigin())))
+
+    # Read the spacing along each dimension
+    spacing = np.array(list(reversed(itkimage.GetSpacing())))
+
+    return ct_scan, origin, spacing
+
+
+class Infer:
+    def __init__(self, trace_path: str, model_path: str, axis: int = -1, slices: int = 20, shape: int = 352,
+                 channels: str = 1):
+        """
+        Inference Model Script
+        :param trace_path:
+        :param axis:
+        :param slices:
+        :param shape
+        :param channels:
+        """
+        self.trace = trace_path
+        self.path = model_path
+        self.axis = axis
+        self.slices = slices
+        self.shape = shape
+        self.channels = channels
+        self.model = torch.jit.load(self.trace)
+        self.model.load_state_dict(torch.load(self.path))
+
+    def predict(self, data: np.ndarray):
+        slices = self.__prepare_data(data)
+        res = self.model(slices)
+        return self.__post_process(res, threshold=0.5)
+
+    def __prepare_data(self, source: np.ndarray):
+        src = np.copy(source)
+        src_shape = src.shape
+        src = zoom(src, (self.shape / src_shape[0], self.shape / src_shape[0], 1))
+        print(src.shape)
+        src = np.expand_dims(src, 0)
+        if self.channels == 1:
+            src = np.repeat(src, 3, 0)
+        print(src.shape)
+        src = np.moveaxis(src, -1, 0)
+        src = torch.Tensor(src)
+        return src
+
+    @staticmethod
+    def __post_process(source, threshold: float):
+        src = source.sigmoid().data.cpu().numpy().squeeze()
+        src = (src - src.min()) / (src.max() - src.min() + 1e-8)
+        src[src <= threshold] = 0.0
+        src[src > threshold] = 1.0
+        src = np.moveaxis(src, 1, -1)
+        return src
