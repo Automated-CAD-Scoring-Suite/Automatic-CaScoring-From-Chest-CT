9d6b9e835938633f40f69db2d01ea52dc10a0e20
Ahmad <ahmedmageed19@gmail.com>
2021-07-15 19:44:08 +0200

    adding Cropping functionality to Scan Parser

--- Models/Segmentation/Parser.py
+++ Models/Segmentation/Parser.py
@@ -5,6 +5,7 @@
 #
 import nibabel as nib
 from scipy.ndimage import zoom, rotate
+from skimage.transform import resize
 import numpy as np
 import cv2
 import os
@@ -15,12 +16,13 @@ train_target_image = 'Data3/Training/image'
 train_target_mask = 'Data3/Training/mask'
 valid_target_image = 'Data3/Validation/image'
 valid_target_mask = 'Data3/Validation/mask'
-
 target = None
 target_image = None
 target_mask = None
 
 PARSE_Train = False
+PARSE_Crop = False
+out_shape = (112, 112, 112)
 
 if PARSE_Train:
     target_image = train_target_image
@@ -30,6 +32,50 @@ else:
     target_image = valid_target_image
     target_mask = valid_target_mask
     target = validation
+
+
+def find_roi_2D(image_slice):
+    # rotate -90
+    s_rotated = np.rot90(image_slice, k=3)
+
+    # flip slice
+    s_fliped = np.flip(image_slice, axis=0)
+    s_rotated_fliped = np.flip(s_rotated, axis=0)
+
+    # Get up and down coordinates
+    y1 = np.unravel_index(np.argmax(image_slice, axis=None), image_slice.shape)
+    y2 = np.unravel_index(np.argmax(s_fliped, axis=None), image_slice.shape)
+
+    x1 = np.unravel_index(np.argmax(s_rotated, axis=None), image_slice.shape)
+    x2 = np.unravel_index(np.argmax(s_rotated_fliped, axis=None), image_slice.shape)
+
+    # return x1, x2, y1, y2 of image
+    return x1[0], image_slice.shape[1] - x2[0], y1[0], image_slice.shape[0] - y2[0]
+
+
+def get_coords(Slices: list):
+    """
+    Returns shape[1] then shape[0]
+    :param Slices: list of 3 slices
+    :return: list [x1,x2,y1,y2]
+    """
+
+    # Initialize coordinates
+    x1 = list()
+    x2 = list()
+    y1 = list()
+    y2 = list()
+
+    # Find ROI in each slice
+    for _slice in Slices:
+        pnt1, pnt2, pnt3, pnt4 = find_roi_2D(_slice)
+        x1.append(pnt1)
+        x2.append(pnt2)
+        y1.append(pnt3)
+        y2.append(pnt4)
+
+    # Return shape[1] then shape[0]
+    return [min(x1), max(x2), min(y1), max(y2)]
 
 
 def zoom3D(img: np.ndarray, factor: float) -> np.ndarray:
@@ -84,14 +130,18 @@ if __name__ == '__main__':
         print(f"Shapes {IMAGING_DATA.shape} {SEGMENT_DATA.shape}", flush=False)
         print(f"Preprocessing .. ", flush=False)
 
+        IMAGING_DATA = range_scale(IMAGING_DATA)
+
         IMAGING_DATA = rotate(IMAGING_DATA, 90)
         SEGMENT_DATA = rotate(SEGMENT_DATA, 90)
 
-        IMAGING_DATA = range_scale(IMAGING_DATA)
+        # Resizing the Volume
+        # IMAGING_DATA = zoom3D(IMAGING_DATA, 2)
+        # SEGMENT_DATA = zoom3D(SEGMENT_DATA, 2)
+        # IMAGING_DATA = resize(IMAGING_DATA, out_shape)
+        # SEGMENT_DATA = resize(SEGMENT_DATA, out_shape)
 
-        IMAGING_DATA = zoom3D(IMAGING_DATA, 2)
-        SEGMENT_DATA = zoom3D(SEGMENT_DATA, 2)
-
+        # Thresholding the PreProcessed Segmentation
         SEGMENT_DATA[SEGMENT_DATA <= 0.5] = 0.0
         SEGMENT_DATA[SEGMENT_DATA > 0.5] = 1.0
 
