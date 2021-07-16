570ec4ac3df3aeef54fc0ba78e3e6b0911b460e4
Salo7a <salo7a98x@gmail.com>
2021-07-16 16:10:42 +0200

    Add Helper Cropping Function Customized For Slicer Volumes

--- Models/crop_roi.py
+++ Models/crop_roi.py
@@ -50,3 +50,39 @@ def get_coords(Slices: list):
 
     # Return shape[1] then shape[0]
     return [min(x1), max(x2), min(y1), max(y2)]
+
+
+def GetCoords(Segmentation, Partial):
+    """
+    Get Cropping Directions In The 3 Planes For The Given Segmentation of Shape (Z, X, Y)
+    """
+    if Partial:
+        # Get Coordinates For Each View
+        AxCoor = [int(i) for i in get_coords(Segmentation[0])]
+        SagCoor = [int(i) for i in get_coords(Segmentation[1])]
+        CorCoor = [int(i) for i in get_coords(Segmentation[2])]
+        CoordinatesList = [AxCoor, SagCoor, CorCoor]
+    else:
+        Z = Segmentation.shape[0] // 2
+        X = Segmentation.shape[1] // 2
+        Y = Segmentation.shape[2] // 2
+        AxCoor = [int(i) for i in get_coords(Segmentation[Z - 1:Z + 2, :, :])]
+        SagCoor = [int(i) for i in get_coords(Segmentation[:, X - 1:X + 2, :])]
+        CorCoor = [int(i) for i in get_coords(Segmentation[:, :, Y - 1, Y + 2])]
+        CoordinatesList = [AxCoor, SagCoor, CorCoor]
+
+    # Coordinates = [[Xmin, Xmax, Ymin, Ymax],[Zmin,Zmax, Ymin, Ymax],[Zmin, Zmax, Xmin,Xmax]]
+
+    # Start Cropping
+    # Determine Correct Cropping Coordinates
+
+    x1 = np.minimum(CoordinatesList[0][0], CoordinatesList[2][0])
+    x2 = np.maximum(CoordinatesList[0][1], CoordinatesList[2][1])
+    y1 = np.minimum(CoordinatesList[0][2], CoordinatesList[1][0])
+    y2 = np.maximum(CoordinatesList[0][3], CoordinatesList[1][1])
+    z1 = np.minimum(CoordinatesList[1][2], CoordinatesList[2][2])
+    z2 = np.maximum(CoordinatesList[1][3], CoordinatesList[2][3])
+
+    Coordinates = [z1, z2, x1, x2, y1, y2]
+
+    return Coordinates
