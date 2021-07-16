5f5eb9266bc69b0d2249f46505b081d64a50283c
Ahmad <ahmedmageed19@gmail.com>
2021-07-16 18:43:53 +0200

    Added Volume Cropping and Nifty Saving

--- Models/Segmentation/Parser.py
+++ Models/Segmentation/Parser.py
@@ -16,13 +16,18 @@ train_target_image = 'Data3/Training/image'
 train_target_mask = 'Data3/Training/mask'
 valid_target_image = 'Data3/Validation/image'
 valid_target_mask = 'Data3/Validation/mask'
+nii_target_file_training = "Data4/Training"
+nii_target_file_validate = "Data4/Validation"
 target = None
 target_image = None
 target_mask = None
 
 PARSE_Train = False
 PARSE_Crop = False
+out_crop = (384, 384, 80)
 out_shape = (112, 112, 112)
+SAVE_Nii = False
+
 
 if PARSE_Train:
     target_image = train_target_image
@@ -32,63 +37,6 @@ else:
     target_image = valid_target_image
     target_mask = valid_target_mask
     target = validation
-
-
-def find_roi_2D(image_slice):
-    # rotate -90
-    s_rotated = np.rot90(image_slice, k=3)
-
-    # flip slice
-    s_fliped = np.flip(image_slice, axis=0)
-    s_rotated_fliped = np.flip(s_rotated, axis=0)
-
-    # Get up and down coordinates
-    y1 = np.unravel_index(np.argmax(image_slice, axis=None), image_slice.shape)
-    y2 = np.unravel_index(np.argmax(s_fliped, axis=None), image_slice.shape)
-
-    x1 = np.unravel_index(np.argmax(s_rotated, axis=None), image_slice.shape)
-    x2 = np.unravel_index(np.argmax(s_rotated_fliped, axis=None), image_slice.shape)
-
-    # return x1, x2, y1, y2 of image
-    return x1[0], image_slice.shape[1] - x2[0], y1[0], image_slice.shape[0] - y2[0]
-
-
-def get_coords(Slices: list):
-    """
-    Returns shape[1] then shape[0]
-    :param Slices: list of 3 slices
-    :return: list [x1,x2,y1,y2]
-    """
-
-    # Initialize coordinates
-    x1 = list()
-    x2 = list()
-    y1 = list()
-    y2 = list()
-
-    # Find ROI in each slice
-    for _slice in Slices:
-        pnt1, pnt2, pnt3, pnt4 = find_roi_2D(_slice)
-        x1.append(pnt1)
-        x2.append(pnt2)
-        y1.append(pnt3)
-        y2.append(pnt4)
-
-    # Return shape[1] then shape[0]
-    return [min(x1), max(x2), min(y1), max(y2)]
-
-
-def zoom3D(img: np.ndarray, factor: float) -> np.ndarray:
-    """
-    Down Sample the input volume to desired shape
-    :param img: Input Img
-    :param factor: Down sampling Factor
-    :return: Zoomed Img
-    """
-    src = np.copy(img)
-    z_factor = (src.shape[-1] * factor) / src.shape[0]
-    src_zoomed = zoom(src, (1 / factor, 1 / factor, 1 / z_factor))
-    return src_zoomed
 
 
 def range_scale(img) -> np.ndarray:
@@ -124,8 +72,11 @@ if __name__ == '__main__':
         print(f"Loading Case {IMAGING_PATH} .. ", flush=False)
 
         # Load the File
-        IMAGING_DATA = nib.load(IMAGING_PATH).get_fdata()
-        SEGMENT_DATA = nib.load(SEGMENT_PATH).get_fdata()
+        IMAGING_NII = nib.load(IMAGING_PATH)
+        SEGMENT_NII = nib.load(SEGMENT_PATH)
+
+        IMAGING_DATA = IMAGING_NII.get_fdata()
+        SEGMENT_DATA = SEGMENT_NII.get_fdata()
 
         print(f"Shapes {IMAGING_DATA.shape} {SEGMENT_DATA.shape}", flush=False)
         print(f"Preprocessing .. ", flush=False)
@@ -136,8 +87,6 @@ if __name__ == '__main__':
         SEGMENT_DATA = rotate(SEGMENT_DATA, 90)
 
         # Resizing the Volume
-        # IMAGING_DATA = zoom3D(IMAGING_DATA, 2)
-        # SEGMENT_DATA = zoom3D(SEGMENT_DATA, 2)
         # IMAGING_DATA = resize(IMAGING_DATA, out_shape)
         # SEGMENT_DATA = resize(SEGMENT_DATA, out_shape)
 
@@ -145,38 +94,77 @@ if __name__ == '__main__':
         SEGMENT_DATA[SEGMENT_DATA <= 0.5] = 0.0
         SEGMENT_DATA[SEGMENT_DATA > 0.5] = 1.0
 
+        # Crop the Images to fit the Second Network
+        if PARSE_Crop:
+            # Get the Volume Midpoint
+            mid_view = IMAGING_DATA.shape[0] // 2
+            mid_depth = IMAGING_DATA.shape[-1] // 2
+            mid_crop_view = out_crop[0] // 2
+            mid_crop_depth = out_crop[-1] // 2
+
+            IMAGING_DATA = IMAGING_DATA[mid_view - mid_crop_view: mid_view + mid_crop_view,
+                                        mid_view - mid_crop_view: mid_view + mid_crop_view,
+                                        mid_depth - mid_crop_depth: mid_depth + mid_crop_depth]
+
+            SEGMENT_DATA = SEGMENT_DATA[mid_view - mid_crop_view: mid_view + mid_crop_view,
+                                        mid_view - mid_crop_view: mid_view + mid_crop_view,
+                                        mid_depth - mid_crop_depth: mid_depth + mid_crop_depth]
+
         # Create the Directory
         if not os.path.exists(target_image):
             os.makedirs(target_image)
 
         if not os.path.exists(target_mask):
             os.makedirs(target_mask)
-
         print(f"Slicing and Dicing .. ", flush=False)
-        for s in range(IMAGING_DATA.shape[-1]):
-            # Slicing the Required Axis
-            z_img = IMAGING_DATA[:, :, s]
-            z_seg = SEGMENT_DATA[:, :, s]
 
-            # Saving as PNG
-            cv2.imwrite(os.path.join(target_image, f"{case}_{s:04}_0.png"), z_img * 255)
-            cv2.imwrite(os.path.join(target_mask, f"{case}_{s:04}_0.png"), z_seg * 255)
+        if SAVE_Nii:
+            # Create Directories if they did not exist
+            if not os.path.exists(nii_target_file_validate):
+                os.makedirs(nii_target_file_validate)
 
-        for s in range(IMAGING_DATA.shape[1]):
-            # Slicing the Required Axis
-            z_img = IMAGING_DATA[:, s, :]
-            z_seg = SEGMENT_DATA[:, s, :]
+            if not os.path.exists(nii_target_file_training):
+                os.makedirs(nii_target_file_training)
 
-            # Saving as PNG
-            cv2.imwrite(os.path.join(target_image, f"{case}_{s:04}_1.png"), z_img * 255)
-            cv2.imwrite(os.path.join(target_mask, f"{case}_{s:04}_1.png"), z_seg * 255)
+            # Create a New NifTi Image
+            IMAGING_NII = nib.Nifti1Image(IMAGING_DATA, IMAGING_NII.affine, IMAGING_NII.header)
+            SEGMENT_NII = nib.Nifti1Image(SEGMENT_DATA, SEGMENT_NII.affine, SEGMENT_NII.header)
+            f_path = None
 
-        for s in range(IMAGING_DATA.shape[0]):
-            # Slicing the Required Axis
-            z_img = IMAGING_DATA[s, :, :]
-            z_seg = SEGMENT_DATA[s, :, :]
+            # Save the Processed Data as Nifty
+            if PARSE_Train:
+                f_path = nii_target_file_training
+            else:
+                f_path = nii_target_file_validate
 
-            # Saving as PNG
-            cv2.imwrite(os.path.join(target_image, f"{case}_{s:04}_2.png"), z_img * 255)
-            cv2.imwrite(os.path.join(target_mask, f"{case}_{s:04}_2.png"), z_seg * 255)
+            nib.save(IMAGING_NII, os.path.join(f_path, f'ct_train_{case}/imaging.nii.gz'))
+            nib.save(SEGMENT_NII, os.path.join(f_path, f'ct_train_{case}/seg_norm.nii.gz'))
+
+        else:
+            for s in range(IMAGING_DATA.shape[-1]):
+                # Slicing the Required Axis
+                z_img = IMAGING_DATA[:, :, s]
+                z_seg = SEGMENT_DATA[:, :, s]
+
+                # Saving as PNG
+                cv2.imwrite(os.path.join(target_image, f"{case}_{s:04}_0.png"), z_img * 255)
+                cv2.imwrite(os.path.join(target_mask, f"{case}_{s:04}_0.png"), z_seg * 255)
+
+            for s in range(IMAGING_DATA.shape[1]):
+                # Slicing the Required Axis
+                z_img = IMAGING_DATA[:, s, :]
+                z_seg = SEGMENT_DATA[:, s, :]
+
+                # Saving as PNG
+                cv2.imwrite(os.path.join(target_image, f"{case}_{s:04}_1.png"), z_img * 255)
+                cv2.imwrite(os.path.join(target_mask, f"{case}_{s:04}_1.png"), z_seg * 255)
+
+            for s in range(IMAGING_DATA.shape[0]):
+                # Slicing the Required Axis
+                z_img = IMAGING_DATA[s, :, :]
+                z_seg = SEGMENT_DATA[s, :, :]
+
+                # Saving as PNG
+                cv2.imwrite(os.path.join(target_image, f"{case}_{s:04}_2.png"), z_img * 255)
+                cv2.imwrite(os.path.join(target_mask, f"{case}_{s:04}_2.png"), z_seg * 255)
         print("Saved !!", flush=False)
