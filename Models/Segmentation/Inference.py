fb13ae8027280f79f9fe47652c8f297a44060522
Ahmad <ahmedmageed19@gmail.com>
2021-07-17 20:18:50 +0200

    Updated Unet Inference Script

--- Models/Segmentation/Inference.py
+++ Models/Segmentation/Inference.py
@@ -6,86 +6,81 @@
 import logging
 
 import numpy as np
-import torch
-from scipy.ndimage import zoom
-import SimpleITK as sitk
-
-
-def load_itk(filename):
-    """
-    This funciton reads a '.mhd' file using SimpleITK and return the image array, origin and spacing of the image.
-
-    :param filename:
-    :return:
-    """
-    # Reads the image using SimpleITK
-    itkimage = sitk.ReadImage(filename)
-
-    # Convert the image to a  numpy array first and then shuffle the dimensions to get axis in the order z,y,x
-    ct_scan = sitk.GetArrayFromImage(itkimage)
-
-    # Read the origin of the ct_scan, will be used to convert the coordinates from world to voxel and vice versa.
-    origin = np.array(list(reversed(itkimage.GetOrigin())))
-
-    # Read the spacing along each dimension
-    spacing = np.array(list(reversed(itkimage.GetSpacing())))
-
-    return ct_scan, origin, spacing
+from skimage.transform import resize
+import tensorflow as tf
 
 
 class Infer:
-    def __init__(self, trace_path: str, model_path: str, axis: int = -1, slices: int = 20, shape: int = 352,
-                 channels: str = 1):
+    def __init__(self, model_path: str, model_input: tuple, threshold: float = 0.9, axial_first: bool = True):
         """
         Inference Model Script
-        :param trace_path:
-        :param axis:
-        :param slices:
-        :param shape
-        :param channels:
+
+        :param model_path: Tensor Flow's SavedModel Format Directory path
+        :param model_input: The Saved Model's Input Shape, used in resizing Input Data
+        :param threshold: Prediction Output Thresholding, useful in case of Binary Prediction
+        :param axial_first: a Boolean Indicator that the Axial Slices appear first in the Input shape,
+                            happens when loading Data in 3D Slicer.
         """
-        self.trace = trace_path
         self.path = model_path
-        self.axis = axis
-        self.slices = slices
-        self.shape = shape
-        self.channels = channels
+
+        self.model_input = model_input
         self.data_shape = None
-        self.model = torch.jit.load(self.trace)
-        if torch.cuda.is_available():
-            logging.info("GPU Processing")
-            self.model.load_state_dict(torch.load(self.path), strict=False)
-        else:
-            logging.info("CPU Processing")
-            self.model.load_state_dict(torch.load(self.path, map_location=torch.device('cpu')), strict=False)
+        self.threshold = threshold
+        self.axial_first = axial_first
+
+        self.model = tf.keras.models.load_model(self.path)
+
+        # if torch.cuda.is_available():
+        #     logging.info("GPU Processing")
+        #     self.model.load_state_dict(torch.load(self.path), strict=False)
+        # else:
+        #     logging.info("CPU Processing")
+        #     self.model.load_state_dict(torch.load(self.path, map_location=torch.device('cpu')), strict=False)
 
     def predict(self, data: np.ndarray):
-        self.data_shape = data.shape
         slices = self.__prepare_data(data)
         res = self.model(slices)
-        return self.__post_process(res, threshold=0.5)
+        return self.__post_process(res)
 
     def __prepare_data(self, source: np.ndarray):
+        # Copying and Saving Input Data Shape
         src = np.copy(source)
-        src_shape = src.shape
-        if len(src_shape) == 2:
-            src = np.expand_dims(src, -1)
-        src = self.range_scale(src)
-        src = zoom(src, (self.shape / src_shape[0], self.shape / src_shape[0], 1))
+
+        # Move the Axial Axis to be the last Dimension to resemble Training Data
+        if self.axial_first:
+            src = np.moveaxis(src, 0, -1)
+        self.data_shape = src.shape
+
+        # Resizing Data to Fit Model.Input,
+        src = resize(src, output_shape=self.model_input)
+
+        # Convert input shape to TF Convention, This is a 3D model
+        # Which needs about 5 Axis (Batches, L, W, D, Channels)
         src = np.expand_dims(src, 0)
-        if self.channels == 1:
-            src = np.repeat(src, 3, 0)
-        src = np.moveaxis(src, -1, 0)
-        src = torch.Tensor(src)
+        src = np.expand_dims(src, -1)
+
+        # Scaling the Image to the Trained Data Scale, Data Is Ready to be Served
+        src = self.range_scale(src)
         return src
 
-    def __post_process(self, source, threshold: float):
-        src = torch.nn.functional.upsample(source, size=self.data_shape, mode='bilinear', align_corners=False)
-        src = src.sigmoid().data.cpu().numpy().squeeze()
-        src = (src - src.min()) / (src.max() - src.min() + 1e-8)
-        src[src <= threshold] = 0.0
-        src[src > threshold] = 1.0
-        src = np.moveaxis(src, 0, -1)
+    def __post_process(self, source):
+        src = np.copy(source)
+
+        # Fix Prediction Shape
+        src = np.squeeze(src, 0)
+        src = np.squeeze(src, -1)
+
+        # Thresholding output Prediction
+        src[src < self.threshold] = 0.0
+        src[src >= self.threshold] = 1.0
+
+        # Upscale to the Input Shape
+        src = resize(src, output_shape=self.data_shape)
+
+        # Fix Return Shape for Slicer
+        if self.axial_first:
+            src = np.moveaxis(src, -1, 0)
+
         return src
 
     @staticmethod
@@ -102,3 +97,21 @@ class Infer:
         src[src > 1] = 1.
         src[src < 0] = 0.
         return src
+
+
+if __name__ == '__main__':
+    import nibabel as nib
+
+    heart_localization = Infer(model_path='./Models_Saved/Heart_Localization/',
+                               model_input=(112, 112, 112),
+                               axial_first=False)
+
+    # load Scans
+    Image = nib.load('./Data/Validation/ct_train_1019/imaging.nii.gz').get_fdata()
+    print(Image.shape)
+
+    # Predict using loaded model
+    pred = heart_localization.predict(Image)
+
+    print(pred.shape)
+    print("Done Inference")
