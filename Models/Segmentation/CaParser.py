0b863da0782c7c7faf1143fd836f9867d88ff043
Ahmad <ahmedmageed19@gmail.com>
2021-07-18 04:10:41 +0200

    Added CAC Data Heart Localization base Script

---
+++ Models/Segmentation/CaParser.py
@@ -0,0 +1,11 @@
+#
+# CaParser.py
+# Author: Ahmad Abdalmageed
+# Date: 7/18/21
+#
+
+import tensorflow as tf
+
+# TODO: 1- Parse Files of CAC and Load Patient Scan,
+#       2- Load Localization model and predict Heart Location,
+#       3- Save Heart Localization
--- Models/Segmentation/Generator.py
+++ Models/Segmentation/Generator.py
@@ -12,7 +12,7 @@ from skimage.exposure import adjust_gamma
 from skimage.transform import resize
 import random
 import SimpleITK as sITK
-
+import math
 
 
 # Augmenter Class
@@ -275,6 +275,46 @@ class CACGen(NiftyGen):
         sITK_ref = sITK.ReadImage(os.path.join(image_path, 'seg_norm.mhd'))
         ref_array = sITK.GetArrayFromImage(sITK_ref)
 
+    def getCubes(self, img, msk, cube_size):
+        sizeX = img.shape[2]
+        sizeY = img.shape[1]
+        sizeZ = img.shape[0]
+
+        cubeSizeX = cube_size[0]
+        cubeSizeY = cube_size[1]
+        cubeSizeZ = cube_size[2]
+
+        n_z = int(math.ceil(float(sizeZ) / cubeSizeZ))
+        n_y = int(math.ceil(float(sizeY) / cubeSizeY))
+        n_x = int(math.ceil(float(sizeX) / cubeSizeX))
+
+        sizeNew = [n_z * cubeSizeZ, n_y * cubeSizeY, n_x * cubeSizeX]
+
+        imgNew = np.zeros(sizeNew, dtype=np.float16)
+        imgNew[0:sizeZ, 0:sizeY, 0:sizeX] = img
+
+        mskNew = np.zeros(sizeNew, dtype=np.int)
+        mskNew[0:sizeZ, 0:sizeY, 0:sizeX] = msk
+
+        n_ges = n_x * n_y * n_z
+        n_4 = int(math.ceil(float(n_ges) / 4.) * 4)
+
+        imgCubes = np.zeros((n_4, cubeSizeZ, cubeSizeY, cubeSizeX)) - 1  # -1 = air
+        mskCubes = np.zeros((n_4, cubeSizeZ, cubeSizeY, cubeSizeX))
+
+        count = 0
+        for z in range(n_z):
+            for y in range(n_y):
+                for x in range(n_x):
+                    imgCubes[count] = imgNew[z * cubeSizeZ:(z + 1) * cubeSizeZ,
+                                      y * cubeSizeY:(y + 1) * cubeSizeY,
+                                      x * cubeSizeX:(x + 1) * cubeSizeX]
+                    mskCubes[count] = mskNew[z * cubeSizeZ:(z + 1) * cubeSizeZ,
+                                      y * cubeSizeY:(y + 1) * cubeSizeY,
+                                      x * cubeSizeX:(x + 1) * cubeSizeX]
+                    count += 1
+
+        return imgCubes, mskCubes, n_x, n_y, n_z
 
 
 if __name__ == '__main__':
