328bc9939778862c64fa97edb0a775a3c12e8b37
Ahmad <ahmedmageed19@gmail.com>
2021-07-19 14:41:16 +0200

    Heart Cropping Script

--- Models/Segmentation/CaParser.py
+++ Models/Segmentation/CaParser.py
@@ -4,8 +4,65 @@
 # Date: 7/18/21
 #
 
-import tensorflow as tf
+import os
+import SimpleITK as sITK
+import Inference
+import numpy as np
 
 # TODO: 1- Parse Files of CAC and Load Patient Scan,
 #       2- Load Localization model and predict Heart Location,
 #       3- Save Heart Localization
+
+parse_train = True
+data_dir = './CaData'
+
+if parse_train:
+    data_dir = os.path.join(data_dir, 'Training')
+else:
+    data_dir = os.path.join(data_dir, 'Validation')
+
+data_files = sorted(os.listdir(data_dir))
+model = Inference.Infer('./Models_Saved/Heart_Localization', model_input=(112, 112, 112))
+
+for patient_scan in data_files:
+    # Load Patient CT Scan
+    patient_dir = os.path.join(data_dir, patient_scan)
+
+    patient_image = sITK.ReadImage(os.path.join(patient_dir, patient_scan+'.mhd'))
+    patient_ref = sITK.ReadImage(os.path.join(patient_dir, patient_scan[:-3]+'r.mhd'))
+
+    patient_image = sITK.GetArrayFromImage(patient_image)
+    patient_ref = sITK.GetArrayFromImage(patient_ref)
+
+    print(f"Loaded Scan with Shape, {patient_image.shape, patient_ref.shape}")
+
+    # Localize the Heart Location
+    heart_location_pred = model.predict(patient_image)
+    print(heart_location_pred.shape)
+
+    # Normalize Heart Reference Data
+    patient_ref[patient_ref > 1] = 1
+    print(np.unique(patient_ref, return_counts=True))
+
+    # Create Bounding Box
+    heart_loc = np.where(heart_location_pred > 0)
+
+    heart_bb = [np.min(heart_loc[0]), np.max(heart_loc[0]),
+                np.min(heart_loc[1]), np.max(heart_loc[1]),
+                np.min(heart_loc[2]), np.max(heart_loc[2])]
+
+    heart_cropped_scan = patient_image[heart_bb[0]: heart_bb[1], heart_bb[2]: heart_bb[3], heart_bb[4]: heart_bb[5]]
+    heart_cropped_ref = patient_ref[heart_bb[0]: heart_bb[1], heart_bb[2]: heart_bb[3], heart_bb[4]: heart_bb[5]]
+
+    # Save Localization
+    heart_image_scan = sITK.GetImageFromArray(heart_cropped_scan)
+    heart_image_ref = sITK.GetImageFromArray(heart_cropped_ref)
+    writer = sITK.ImageFileWriter()
+
+    writer.SetFileName(os.path.join(patient_dir, patient_scan+'h.mhd'))
+    writer.Execute(heart_image_scan)
+
+    writer.SetFileName(os.path.join(patient_dir, patient_scan[:-3]+'rh.mhd'))
+    writer.Execute(heart_image_ref)
+
+print("Done Cropping!!")
