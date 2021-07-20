26bf3ed36f6e6984c01f16d96e243ef1a8db3d88
Ahmad <ahmedmageed19@gmail.com>
2021-07-20 04:27:05 +0200

    main file update

--- Models/Segmentation/main.py
+++ Models/Segmentation/main.py
@@ -6,7 +6,7 @@ import os
 import tensorflow as tf
 from Unet.unet import UNet
 import callbacks as cb
-from functions import dice_coef_loss, dice_coef
+from functions import dice_coef_loss, dice_coef, tversky_index_loss, tversky_index
 from Generator import NiftyGen, NiftyAugmentor, CACGen
 import nibabel as nib
 from skimage.transform import resize
@@ -42,16 +42,16 @@ if gpus:
 mode = '3D'
 down_factor = True
 output_shape = (112, 112, 112)
-input_shape = (112, 112, 112, 1)
+input_shape = (32, 64, 64, 1)
 levels = 4
 kernel_size = (3, 3, 3)
 convolutions = 2
 initial_features = 32
 
 batch_norm = True
-drop_out = None
+drop_out = 0.5
 activation = 'relu'
-lr = 0.001
+lr = 0.0001
 optimizer = tf.keras.optimizers.Adam(lr)
 ####################################################################################################################
 # Augmentation Parameters
@@ -66,7 +66,7 @@ gen_val = NiftyGen(validation, augmenter=None, mode=mode, output_shape=output_sh
 ca_gen = CACGen(CA_training, augmenter=aug, mode=mode, output_shape=output_shape, down_factor=down_factor, save=False,
                 shuffle=False)
 ca_val = CACGen(CA_validation, augmenter=None, mode=mode, output_shape=output_shape, down_factor=down_factor,
-                  shuffle=False)
+                shuffle=False)
 
 # MODEL INSTANTIATION
 uNet3D = UNet("conv3D", up_sample="upSample3D", transpose=None, pool="max3D")
@@ -82,7 +82,7 @@ print(model.summary())
 callbacks = [
     cb.GarbageCollect(),
     tf.keras.callbacks.ModelCheckpoint(filepath=f'{weights_path}/{model.name}_checkpoint.h5', save_freq='epoch'),
-    tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.001, patience=20),
+    tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.0001, patience=20),
     tf.keras.callbacks.ReduceLROnPlateau(),
     tf.keras.callbacks.CSVLogger(f'{model.name}_logs_CAC.csv'),
     cb.DisplayCallback()
@@ -92,6 +92,8 @@ if not Testing:
     print("Training on ", gpus[0].name)
     # Training
     model.compile(optimizer=optimizer, loss=dice_coef_loss, metrics=['accuracy', dice_coef])
+    # model.compile(optimizer=optimizer, loss=tversky_index_loss,
+    #               metrics=['accuracy','sparse_categorical_accuracy',tversky_index])
     print(f"Training {model.name}")
 
     with tf.device("/" + gpus[0].name[10:]):
@@ -134,18 +136,24 @@ if Testing:
     # Segmented = Segmented_nii.get_fdata()
 
     # Load Patient's Scans
-    sITK_image = sITK.ReadImage('./Test/trv1p1ctai.mhd')
+    sITK_image = sITK.ReadImage('./CaData/Validation/trv4p4cti/trv4p4ctih.mhd')
     Image = sITK.GetArrayFromImage(sITK_image)
 
-    # Test rotating Input
-    # Image = rotate(Image, 90)
+    sITK_Ref = sITK.ReadImage('./CaData/Validation/trv4p4cti/trv4p4rh.mhd')
+    Ref = sITK.GetArrayFromImage(sITK_Ref)
 
-    Image = np.moveaxis(Image, 0, -1)
-
-    print(Image.min(), Image.max())
-
-    # Reshape Both Tensors
+    print(np.unique(Ref, return_counts=True))
+    print(Image.shape, Ref.shape)
+    # # Test rotating Input
+    # # Image = rotate(Image, 90)
+    #
+    # Image = np.moveaxis(Image, 0, -1)
+    #
+    # print(Image.min(), Image.max())
+    #
+    # # Reshape Both Tensors
     Image = resize(Image, output_shape=output_shape, preserve_range=True)
+    Ref = resize(Image, output_shape=output_shape, preserve_range=True)
     print(f"Data Shape , {Image.shape}")
 
     Image = np.expand_dims(Image, 0)
@@ -154,7 +162,7 @@ if Testing:
     # Range Scale
     Image = range_scale(Image)
 
-    # # Predict Using loaded Parameters
+    # Predict Using loaded Parameters
     pred = model.predict(Image)
 
     Image = np.squeeze(Image, 0)
@@ -163,24 +171,24 @@ if Testing:
     pred = np.squeeze(pred, 0)
     pred = np.squeeze(pred, -1)
 
-    # print(pred.shape)
-    pred[pred < 0.9] = 0.0
-    pred[pred >= 0.9] = 1.0
+    print(pred.shape)
+    # pred[pred < 0.5] = 0.0
+    # pred[pred >= 0.9] = 1.0
 
     print(np.unique(pred, return_counts=True))
-    cv2.imwrite('test_pred.png', pred[:, 90, :] * 255)
-    cv2.imwrite('test_img.png', Image[:, 90, :] * 255)
-
-    pred_mhd = sITK.GetImageFromArray(pred)
-
-    writer = sITK.ImageFileWriter()
-    writer.SetFileName('test_pred_mhd_rot.mhd')
-    writer.Execute(pred_mhd)
-
-    # img_nii_test = nib.Nifti1Image(Image, Image_nii.affine, Image_nii.header)
-    # seg_nii_test = nib.Nifti1Image(pred, Segmented_nii.affine, Segmented_nii.header)
-    # nib.save(seg_nii_test, './test_pred.nii.gz')
-    # nib.save(img_nii_test, './test_Image.nii.gz')
+    # cv2.imwrite('test_pred.png', pred[:, 90, :] * 255)
+    # cv2.imwrite('test_img.png', Image[:, 90, :] * 255)
+    #
+    # pred_mhd = sITK.GetImageFromArray(pred)
+    #
+    # writer = sITK.ImageFileWriter()
+    # writer.SetFileName('test_pred_mhd_rot.mhd')
+    # writer.Execute(pred_mhd)
+    #
+    # # img_nii_test = nib.Nifti1Image(Image, Image_nii.affine, Image_nii.header)
+    # # seg_nii_test = nib.Nifti1Image(pred, Segmented_nii.affine, Segmented_nii.header)
+    # # nib.save(seg_nii_test, './test_pred.nii.gz')
+    # # nib.save(img_nii_test, './test_Image.nii.gz')
 
 if Deployment:
     # Compile Created Model
