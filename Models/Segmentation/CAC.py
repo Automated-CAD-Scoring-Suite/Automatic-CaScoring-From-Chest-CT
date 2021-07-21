4c162fb8df0511f80f3d6455cb14378cea1cb6de
Salo7a <salo7a98x@gmail.com>
2021-07-22 01:50:52 +0200

    Added Missing CAC Functions

--- Models/Segmentation/CAC.py
+++ Models/Segmentation/CAC.py
@@ -3,3 +3,18 @@
 # Author: Ahmad Abdalmageed
 # Date: 7/22/21
 #
+import numpy as np
+
+
+def ThresholdCAC(scan: np.ndarray, threshold: float = 130) -> np.ndarray:
+    src = np.copy(scan)
+    src[src < threshold] = 0
+    src[src >= threshold] = 1
+    return src
+
+
+def QuantifyCAC(scan_threshold: np.ndarray, pred: np.ndarray, voxel_spacing: list) -> float:
+    masked_out = scan_threshold * pred
+    candidate_voxels = np.count_nonzero(masked_out)
+    voxel_vol = (voxel_spacing[0]*voxel_spacing[1]*voxel_spacing[2]) / 3
+    return candidate_voxels*voxel_vol
