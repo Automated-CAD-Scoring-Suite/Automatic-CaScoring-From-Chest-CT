ab88a2fd06a941402bf4d1540049dcd0c0fcf6f3
Ahmad <ahmedmageed19@gmail.com>
2022-12-05 15:33:04 +0200

    update(parsers): update CaParser functionality

--- Models/Segmentation/CaParser.py
+++ Models/Segmentation/CaParser.py
@@ -8,8 +8,9 @@ import os
 import SimpleITK as sITK
 import Inference
 import numpy as np
-import tensorflow.keras.backend as bck
+import tensorflow.keras as bck
 from skimage.morphology import binary_dilation
+from CAC import ThresholdCAC, QuantifyCAC
 
 # TODO: 1- Parse Files of CAC and Load Patient Scan,
 #       2- Load Localization model and predict Heart Location,
@@ -32,7 +33,7 @@ else:
     data_dir = os.path.join(data_dir, 'Validation')
 
 data_files = sorted(os.listdir(data_dir))
-model = Inference.Infer('./Models_Saved/Heart_Localization', model_input=(112, 112, 112))
+model = Inference.Infer('./Models_Saved/Heart_Localization', model_input=(112, 112, 112), axial_first=True)
 
 # for patient_scan in data_files:
 #     # Load Patient CT Scan
@@ -75,41 +76,52 @@ model = Inference.Infer('./Models_Saved/Heart_Localization', model_input=(112, 1
 #     writer.SetFileName(os.path.join(patient_dir, patient_scan[:-3]+'rh.mhd'))
 #     writer.Execute(heart_image_ref)
 
+error = []
+dc = []
 for patient_scan in data_files:
     # Load Patient CT Scan
     patient_dir = os.path.join(data_dir, patient_scan)
 
-    print("Loading Patient , ", patient_scan)
+    print(f"Loading Patient {patient_scan} and Calculation Started")
+
     patient_image = sITK.ReadImage(os.path.join(patient_dir, patient_scan+'.mhd'))
     patient_ref = sITK.ReadImage(os.path.join(patient_dir, patient_scan[:-3]+'r.mhd'))
 
+    spacing = patient_image.GetSpacing()
+    # print(f"Spacing, {spacing}")
+
     patient_image = sITK.GetArrayFromImage(patient_image)
     patient_ref = sITK.GetArrayFromImage(patient_ref)
-    print(patient_image.min(), patient_image.max())
 
     patient_ref[patient_ref > 1] = 1
-    print(np.unique(patient_ref, return_counts=True))
-    patient_image = ((np.clip(patient_image, -1024.0, 3071.0)) - 1023.5) / 2047.5
-    print(patient_image.min(), patient_image.max())
-    print(patient_ref.min(), patient_ref.max())
 
-    dc = []
-    pred_mask = np.zeros((patient_image.shape))
-    dc.append(dice_coef(patient_ref, pred_mask))
-    print(np.mean(dc))
+    # print(patient_image.min(), patient_image.max())
+    # print(np.unique(patient_ref, return_counts=True))
 
-    # print("Reference ", np.unique(patient_ref, return_counts=True))
-    #
-    # # Prediction
-    # heart = model.predict(patient_image)
-    # print("Heart", np.unique(heart, return_counts=True))
-    #
-    # # Processing Input Patient Scan
-    # patient_image = heart * patient_image
-    # patient_image[patient_image < 130] = 0
-    # patient_image[patient_image >= 130] = 1
-    # patient_image = binary_dilation(patient_image).astype('int16')
-    #
+    # Prediction
+    heart_location_pred = model.predict(patient_image)
+
+    # Crop Volume
+    # Create Bounding Box
+    heart_loc = np.where(heart_location_pred > 0)
+
+    heart_bb = [np.min(heart_loc[0]), np.max(heart_loc[0]),
+                np.min(heart_loc[1]), np.max(heart_loc[1]),
+                np.min(heart_loc[2]), np.max(heart_loc[2])]
+
+    heart_cropped_scan = patient_image[heart_bb[0]: heart_bb[1], heart_bb[2]: heart_bb[3], heart_bb[4]: heart_bb[5]]
+    heart_cropped_ref = patient_ref[heart_bb[0]: heart_bb[1], heart_bb[2]: heart_bb[3], heart_bb[4]: heart_bb[5]]
+    heart_loc_cropped = heart_location_pred[heart_bb[0]: heart_bb[1], heart_bb[2]: heart_bb[3], heart_bb[4]: heart_bb[5]]
+
+    scan_threshold = ThresholdCAC(heart_cropped_scan, 160)
+
+    pred, vol_pred = QuantifyCAC(scan_threshold, heart_loc_cropped, spacing)
+    _, vol_ref = QuantifyCAC(heart_cropped_ref, heart_loc_cropped, spacing)
+
+    error.append((vol_ref - vol_pred) ** 2)
+    dc.append(dice_coef(heart_cropped_ref, scan_threshold))
+
+    # Processing Input Patient Scan
     # print("CAC ", np.unique(patient_image, return_counts=True))
     #
     # # Calculate Dice Coefficient
@@ -126,5 +138,7 @@ for patient_scan in data_files:
 
     # writer.SetFileName(os.path.join(patient_dir, patient_scan+'test.mhd'))
     # writer.Execute(ref_norm)
-
-print("Done Cropping!!")
+print(np.mean(error))
+print(np.sqrt(np.mean(error)))
+print(np.mean(dc))
+print("Done!!")
